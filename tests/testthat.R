761972560eb04e37e24b6c99b859eb8615291c11
Matthew Kay <matthew.kay@gmail.com>
2015-04-17 20:51:56 -0700

    basic tests for constructors and extract_samples

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(tidybayes)
+
+test_check("tidybayes")
---
+++ tests/testthat/test.extract_samples.R
@@ -0,0 +1,151 @@
+# Tests for extract_samples
+# 
+# Author: mjskay
+###############################################################################
+
+library(testthat)
+library(plyr)
+library(dplyr)
+library(tidyr)
+library(tidybayes)
+
+
+test_that("extract_samples on a simple parameter with no indices", {
+        data(RankCorr, package="tidybayes")
+        
+        ref = data.frame(
+            .sample = 1:nrow(RankCorr),
+            typical_r = RankCorr[,"typical_r"]
+            )
+        expect_equal(extract_samples(RankCorr, typical_r[]), ref)
+    })
+
+
+test_that("extract_samples works on a parameter with one unnamed index", {
+        data(RankCorr, package="tidybayes")
+        
+        ref = ldply(1:18, function(i) {
+                data.frame(
+                    .sample = 1:nrow(RankCorr),
+                    i = i,
+                    tau = RankCorr[,paste0("tau[", i, "]")]
+                    )
+            })
+        
+        expect_equal(extract_samples(RankCorr, tau[i]), ref)
+    })
+
+test_that("extract_samples works on a parameter with one named index", {
+        i_labels = c("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r")
+        data(RankCorr, package="tidybayes")
+        RankCorr = apply_prototypes(RankCorr, list(i=factor(i_labels)))
+        
+        ref = ldply(1:18, function(i) {
+                data.frame(
+                    .sample = 1:nrow(RankCorr),
+                    i = i_labels[i],
+                    tau = RankCorr[,paste0("tau[", i, "]")]
+                )
+            })
+        
+        expect_equal(extract_samples(RankCorr, tau[i]), ref)
+    })
+
+test_that("extract_samples works on a parameter with one anonymous wide index", {
+        data(RankCorr, package="tidybayes")
+        
+        ref = data.frame(.sample = 1:nrow(RankCorr)) 
+        for (i in 1:18) {
+            refcol = data.frame(RankCorr[,paste0("tau[", i, "]")])
+            names(refcol) = paste0("tau", i)
+            ref = cbind(ref, refcol)
+        }
+        
+        expect_equal(extract_samples(RankCorr, tau[..]), ref)
+    })
+
+
+test_that("extract_samples works on a parameter with one named wide index", {
+        i_labels = c("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r")
+        data(RankCorr, package="tidybayes")
+        RankCorr = apply_prototypes(RankCorr, list(i=factor(i_labels)))
+        
+        ref = data.frame(.sample = 1:nrow(RankCorr)) 
+        for (i in 1:18) {
+            refcol = data.frame(RankCorr[,paste0("tau[", i, "]")])
+            names(refcol) = i_labels[i]
+            ref = cbind(ref, refcol)
+        }
+        
+        expect_equal(extract_samples(RankCorr, tau[i] | i), ref)
+    })
+
+
+test_that("extract_samples works on a parameter with two named indices", {
+        i_labels = c("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r")
+        j_labels = c("A","B","C","D")
+        data(RankCorr, package="tidybayes")
+        RankCorr = apply_prototypes(RankCorr, 
+            list(i = factor(i_labels), j = factor(j_labels)))
+        
+
+        ref = ldply(1:4, function(j) {
+                ldply(1:18, function(i) {
+                    data.frame(
+                        .sample = 1:nrow(RankCorr),
+                        i = i_labels[i],
+                        j = j_labels[j],
+                        b = RankCorr[,paste0("b[", i, ",", j, "]")]
+                    )
+                })
+            })
+        
+        expect_equal(extract_samples(RankCorr, b[i,j]), ref)
+    })
+
+
+test_that("extract_samples works on a parameter with two named indices, one that is wide", {
+        i_labels = c("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r")
+        j_labels = c("A","B","C","D")
+        data(RankCorr, package="tidybayes")
+        RankCorr = apply_prototypes(RankCorr, 
+            list(i = factor(i_labels), j = factor(j_labels)))
+        
+        
+        ref = ldply(1:4, function(j) {
+                ldply(1:18, function(i) {
+                    data.frame(
+                        .sample = 1:nrow(RankCorr),
+                        i = i_labels[i],
+                        j = j_labels[j],
+                        b = RankCorr[,paste0("b[", i, ",", j, "]")]
+                    )
+                })
+            }) %>%
+            spread(j, b)
+        
+        expect_equal(extract_samples(RankCorr, b[i,j] | j), ref)
+    })
+
+test_that("extract_samples works on a parameter with one named index and one wide anonymous index", {
+        i_labels = c("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r")
+        data(RankCorr, package="tidybayes")
+        RankCorr = apply_prototypes(RankCorr, 
+            list(i = factor(i_labels)))
+        
+        
+        ref = ldply(1:4, function(j) {
+                ldply(1:18, function(i) {
+                    data.frame(
+                        .sample = 1:nrow(RankCorr),
+                        i = i_labels[i],
+                        j = paste0("b", j),
+                        b = RankCorr[,paste0("b[", i, ",", j, "]")]
+                    )
+                })
+            }) %>%
+            spread(j, b)
+        
+        expect_equal(extract_samples(RankCorr, b[i,..]), ref)
+    })
+
