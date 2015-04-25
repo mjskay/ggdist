512863cafdbd176f98c1a1b02c279b73657c208d
Matthew Kay <matthew.kay@gmail.com>
2015-04-25 11:27:15 +0900

    initial code for extracting multiple variables simultaneously

--- R/extract_samples.R
+++ R/extract_samples.R
@@ -4,6 +4,19 @@
 ###############################################################################
 
 
+#helper function to parse variable names (as an expression where
+#they are separated by "+") into a list
+parse_variable_names = function(quoted_variable_names) UseMethod("parse_variable_names")
+parse_variable_names.default = function(quoted_variable_names) as.character(quoted_variable_names)
+parse_variable_names.call = function(quoted_variable_names) {
+    unlist(lapply(quoted_variable_names[-1], #drop the function call name (params only)
+            parse_variable_names))
+}
+`parse_variable_names.(` = function(quoted_variable_names) {
+    parse_variable_names(quoted_variable_names[[2]])
+}
+
+
 ## Extract a sample from an MCMC chain for a variable with the given named indices into a long-format data frame.
 ## For example, imagine a variable b[i,v] with i in [1..100] and v in [1..3]. An MCMC sample returned from JAGS 
 ## (for example) would have columns with names like "b[1,1]", "b[2,1]", etc. 
@@ -47,8 +60,8 @@ extract_samples = function(model, variable_spec) {
 extract_samples_ = function(model, variable_spec) {
     #parse a variable spec in the form variable_name[index_name_1, index_name_2, ..] | wide_index
     spec = lazy_eval(variable_spec, data=list(
-        `[` = function(variable_name, ...) list(
-            as.character(substitute(variable_name)),
+        `[` = function(variable_names, ...) list(
+            parse_variable_names(substitute(variable_names)),
             as.character(substitute(list(...))[-1]),
             NA),
         `|` = function(spec, by) c(
@@ -56,35 +69,42 @@ extract_samples_ = function(model, variable_spec) {
             as.character(substitute(by))
             )
         ))
-    variable_name = spec[[1]]
+    variable_names = spec[[1]]
     index_names = if (identical(spec[[2]], "")) NULL else spec[[2]]
     wide_index_name = spec[[3]]
     
     #extract the samples into a long data frame
-    samples = extract_samples_long_(model, variable_name, index_names)
+    samples = extract_samples_long_(model, variable_names, index_names)
     
     #convert variable and/or indices back into usable data types
     constructors = attr(model, "constructors")
     if (is.null(constructors)) constructors = list()
-    for (column_name in c(variable_name, index_names)) {
+    for (column_name in c(variable_names, index_names)) {
         if (column_name %in% names(constructors)) {
             #we have a data type constructor for this index, convert it
             samples[[column_name]] = constructors[[column_name]](samples[[column_name]])
         }
     }
     
-    #spread a column into wide format if requested
-    if (!is.na(wide_index_name)) {  
+    #spread a column into wide format if requested (only if one variable, because
+    #we can't spread multiple keys simultaneously for the same value)
+    if (!is.na(wide_index_name)) {
         #wide index requested by name
+        if (length(variable_names) != 1) {
+            stop("Cannot extract samples of multiple variables in wide format.")
+        }
         samples %>%
-            spread_(wide_index_name, variable_name)
+            spread_(wide_index_name, variable_names)
     }
     else if (!is.null(samples$..)) {
         #a column named ".." is present, use it to form a wide version of the data
         #with numbered names based on the variable name
+        if (length(variable_names) != 1) {
+            stop("Cannot extract samples of multiple variables in wide format.")
+        }
         samples %>%
-            mutate(.. = factor(.., labels=variable_name)) %>%
-            spread_("..", variable_name)
+            mutate(.. = factor(.., labels=variable_names)) %>%
+            spread_("..", variable_names)
     }
     else {
         #no wide column => just return long version
@@ -103,27 +123,33 @@ extract_samples_ = function(model, variable_spec) {
 ##		column "v":		  values in [1..3]
 ##      column "b":       sample number ".sample" of "b[i,v]" in mcmcChain 
 ##
-extract_samples_long_ = function(model, variable_name, index_names) {
+extract_samples_long_ = function(model, variable_names, index_names) {
     if (is.null(index_names)) {
         #no indices, just return the samples with a sample index added
-        values = list(model[,variable_name])
-        names(values) = variable_name
+        values = list(model[,variable_names])
+        names(values) = variable_names
         data.frame(.sample=1:nrow(model), values)
     }
     else {
-        ldply(colnames(model), function(colname) {
-            #parse column into variable name and indices
-            colname_parts = strsplit(colname,"\\,|\\]|\\[")[[1]]
-            if (colname_parts[1] == variable_name) {	#this is the variable we want
-                #get the values of the indices 
-                indices = as.list(as.numeric(colname_parts[-1]))
-                names(indices) = index_names
-                #get the values of this variable in each sample
-                values = list(model[,colname])
-                names(values) = variable_name
-                #put it all together
-                data.frame(.sample=1:nrow(model), indices, values)
-            }
-        })
+        all_samples = NULL
+        for (variable_name in variable_names) { #TODO: this is ugly and could be more efficient
+            variable_samples = ldply(colnames(model), function(colname) {
+                #parse column into variable name and indices
+                colname_parts = strsplit(colname,"\\,|\\]|\\[")[[1]]
+                if (colname_parts[1] == variable_name) {	#this is the variable we want
+                    #get the values of the indices 
+                    indices = as.list(as.numeric(colname_parts[-1]))
+                    names(indices) = index_names
+                    #get the values of this variable in each sample
+                    values = list(model[,colname])
+                    names(values) = variable_name
+                    #put it all together
+                    data.frame(.sample=1:nrow(model), indices, values)
+                }
+            })
+            all_samples = if (is.null(all_samples)) variable_samples
+                else left_join(all_samples, variable_samples, by=intersect(names(all_samples), names(variable_samples))) 
+        }
+        all_samples
     }
 }
