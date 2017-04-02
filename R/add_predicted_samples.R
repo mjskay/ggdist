f0ed2b9dc0b021ea54a63a9b7f6bffa02171adf6
Matthew Kay <matthew.kay@gmail.com>
2017-04-02 14:26:03 -0400

    add_fitted_samples for stanreg, towards #33

--- R/add_predicted_samples.R
+++ R/add_predicted_samples.R
@@ -8,16 +8,26 @@
 globalVariables(c(".iteration", ".pred"))
 
 
-#' Add samples from the posterior prediction from a model to a data frame
+#' Add samples from the posterior fit or posterior prediction of a model to a data frame
 #' 
-#' Given a data frame, adds samples from the posterior predictions of the model to the data
-#' in a long format.
+#' Given a data frame, adds samples from the posterior fit (aka the linear/link-level predictor) 
+#' or the posterior predictions of the model to the data in a long format.
 #' 
-#' \code{add_predicted_samples} and \code{predict_samples} are alternate spellings of the 
+#' \code{add_fitted_samples} adds samples from the posterior linear predictor (or the "link") to
+#' the data. It corresponds to \code{\link[rstanarm]{posterior_linpred}} in \code{rstanarm} or
+#' \code{\link[brms]{fitted.brmsfit}} in \code{brms}.
+#' 
+#' \code{add_predicted_samples} adds samples from the posterior prediction to
+#' the data. It corresponds to \code{\link[rstanarm]{posterior_predict}} in \code{rstanarm} or
+#' \code{\link[brms]{predict.brmsfit}} in \code{brms}.
+#' 
+#' \code{add_fitted_samples} and \code{fitted_samples} are alternate spellings of the 
 #' same function with opposite order of the first two arguments to facilitate use in data
-#' processing pipelines that start either with a data frame or a model.
+#' processing pipelines that start either with a data frame or a model. Similarly,
+#' \code{add_predicted_samples} and \code{predicted_samples} are alternate spellings.
 #' 
-#' Given equal choice between the two, \code{add_predicted_samples} is the preferred spelling.
+#' Given equal choice between the two, \code{add_fitted_samples} and \code{add_predicted_samples}
+#' are the preferred spellings.
 #' 
 #' @param newdata Data frame to generate predictions from. If omitted, most model types will
 #' generate predictions from the data used to fit the model.
@@ -32,7 +42,8 @@ globalVariables(c(".iteration", ".pred"))
 #' factor grouping rows from the input \code{newdata}), \code{.chain} column (the chain
 #' each sample came from, or \code{NA} if the model does not provide chain information),
 #' \code{.iteration} column (the iteration the sample came from), and \code{.pred} column (a
-#' sample from the posterior predictive distribution).
+#' sample from the posterior predictive distribution). For convenience, the resulting data
+#' frame comes grouped by the original input rows.
 #' @author Matthew Kay
 #' @seealso \code{\link{gather_samples}}
 #' @keywords manip
@@ -41,34 +52,44 @@ globalVariables(c(".iteration", ".pred"))
 #' @importFrom dplyr mutate
 #' @export
 add_predicted_samples = function(newdata, model, ...) {
-    predict_samples(model, newdata, ...)
-}
-    
-#' @rdname add_predicted_samples
-#' @export
-predict_samples = function(model, newdata, ...) UseMethod("predict_samples")
-
-#' @rdname add_predicted_samples
-#' @export
-predict_samples.default = function(model, newdata, ...) {
-    stop(paste0("Models of type ", deparse0(class(model)), " are not currently supported by add_predicted_samples and predict_samples"))
+    predicted_samples(model, newdata, ...)
 }
 
 #' @rdname add_predicted_samples
 #' @export
-predict_samples.stanreg = function(model, newdata, ...) {
-    if (!requireNamespace("rstan", quietly = TRUE)) {
-        stop('The `rstantools` package is needed for `predict_samples` to support `stanreg` objects.'
-            , call. = FALSE)
-    }
-    
+add_fitted_samples = function(newdata, model, ...) {
+    fitted_samples(model, newdata, ...)
+}
+
+#' @rdname add_predicted_samples
+#' @export
+predicted_samples = function(model, newdata, ...) UseMethod("predicted_samples")
+
+#' @rdname add_predicted_samples
+#' @export
+fitted_samples = function(model, newdata, ...) UseMethod("fitted_samples")
+
+#' @rdname add_predicted_samples
+#' @export
+predicted_samples.default = function(model, newdata, ...) {
+    stop(paste0("Models of type ", deparse0(class(model)), " are not currently supported by `predicted_samples`"))
+}
+
+#' @rdname add_predicted_samples
+#' @export
+fitted_samples.default = function(model, newdata, ...) {
+    stop(paste0("Models of type ", deparse0(class(model)), " are not currently supported by `fitted_samples`"))
+}
+
+# template for predicted_samples.stanreg and fitted_samples.stanreg
+fitted_predicted_samples_stanreg_ = function(f_fitted_predicted, model, newdata, ...) {
     newdata %>%
         data.frame(
             #for some reason calculating row here instead of in a subsequent mutate()
             #is about 3 times faster
             .row = factor(1:nrow(.)),
             .chain = as.numeric(NA),
-            t(rstantools::posterior_predict(model, newdata = .)), 
+            t(f_fitted_predicted(model, newdata = ., ...)), 
             check.names=FALSE
         ) %>%
         gather(.iteration, .pred, (ncol(newdata)+3):ncol(.)) %>%
@@ -76,4 +97,26 @@ predict_samples.stanreg = function(model, newdata, ...) {
             .iteration = as.numeric(.iteration)
         ) %>%
         group_by_(".row", .dots = colnames(newdata))
+}
+
+#' @rdname add_predicted_samples
+#' @export
+predicted_samples.stanreg = function(model, newdata, ...) {
+    if (!requireNamespace("rstantools", quietly = TRUE)) {
+        stop('The `rstantools` package is needed for `predicted_samples` to support `stanreg` objects.'
+            , call. = FALSE)
+    }
+
+    fitted_predicted_samples_stanreg_(rstantools::posterior_predict, model, newdata, ...)
+}
+
+#' @rdname add_predicted_samples
+#' @export
+fitted_samples.stanreg = function(model, newdata, ...) {
+    if (!requireNamespace("rstanarm", quietly = TRUE)) {
+        stop('The `rstanarm` package is needed for `fitted_samples` to support `stanreg` objects.'
+            , call. = FALSE)
+    }
+    
+    fitted_predicted_samples_stanreg_(rstanarm::posterior_linpred, model, newdata, ...)
 }
