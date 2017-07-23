8f547c7b26e1f9afa533f50ddc8bbbfc3cdda397
Matthew Kay <matthew.kay@gmail.com>
2017-07-23 12:47:01 -0400

    drop old/unused contrast_levels.R

--- R/contrast_levels.R
+++
@@ -1,9 +0,0 @@
-# contrast_levels
-# 
-# Author: mjskay
-###############################################################################
-
-##TODO: write this
-## some helpful things
-## t(pairwise.lsmc(c("a","b","c"))) %*% contr.treatment(c("a","b","c"))
-## llply(as.data.frame(t(t(pairwise.lsmc(c("a","b","c"))) %*% contr.treatment(c("a","b","c")))), function(i) i)
