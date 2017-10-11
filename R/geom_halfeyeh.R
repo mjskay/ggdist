12fa8dd3eda9748067737a9e17a3067e8bbdc111
Matthew Kay <matthew.kay@gmail.com>
2017-10-10 23:49:33 -0400

    ggjoy -> ggridges

--- R/geom_halfeyeh.R
+++ R/geom_halfeyeh.R
@@ -7,7 +7,7 @@
 
 #' Half-eye plots of densities with point estimates and intervals (ggplot geom)
 #'
-#' Generates a combination geom_joy and geom_pointrangeh (using stat_summaryh)
+#' Generates a combination geom_density_ridges and geom_pointrangeh (using stat_summaryh)
 #' representing the density, point estimates, and credible interval. Useful
 #' for representing posterior estimates from Bayesian samplers; in that context
 #' the mirrored verison is variously called an eye plot, a raindrop plot, or a
@@ -17,7 +17,7 @@
 #' used (under various names and with subtle variations) to visualize posterior
 #' distributions in Bayesian inference. This instantiation is a combination of
 #' a density plot, point estimate, and credible interval. \code{geom_halfeyeh()} is
-#' equivalent to \code{geom_joy() + stat_summaryh()} with some reasonable
+#' equivalent to \code{geom_density_ridges() + stat_summaryh()} with some reasonable
 #' defaults, including color choices and the use of mean with 95\% quantile
 #' intervals.
 #'
@@ -26,11 +26,11 @@
 #' layer level if you are overriding the plot defaults.
 #' @param data A layer specific dataset - only needed if you want to override
 #' the plot defaults.
-#' @param stat Passed to \code{\link{geom_joy}}. The statistical
+#' @param stat Passed to \code{\link{geom_density_ridges}}. The statistical
 #' transformation to use on the data for this layer.
-#' @param position Passed to \code{\link{geom_joy}}. The position adjustment
+#' @param position Passed to \code{\link{geom_density_ridges}}. The position adjustment
 #' to use for overlapping points on this layer.
-#' @param scale Passed to \code{\link{geom_joy}}. A scaling factor to scale the
+#' @param scale Passed to \code{\link{geom_density_ridges}}. A scaling factor to scale the
 #' height of the densities relative to the spacing between them. A value of 1
 #' indicates that the maximum point of any density touches the baseline right
 #' above, assuming even spacing between baselines.
@@ -38,7 +38,7 @@
 #' The cutoff is measured relative to the overall maximum, so rel_min_height=0.01
 #' would remove everything that is 1% or less than the highest point among all
 #' densities. Default is 0, so nothing is removed.
-#' @param fill Passed to \code{\link{geom_joy}}. Fill color of the density.
+#' @param fill Passed to \code{\link{geom_density_ridges}}. Fill color of the density.
 #' @param ...  Currently unused.
 #' @param fun.data A function that is given a vector and should
 #'   return a data frame with variables \code{x}, \code{xmin} and \code{xmax}
@@ -56,7 +56,7 @@
 #' estimate and credible interval.
 #' @author Matthew Kay
 #' @seealso See \code{\link{geom_eye}} and \code{\link{geom_eyeh}} for the mirrored-density
-#' (full "eye") versions. See \code{\link{geom_joy}} and \code{\link{stat_summaryh}} for the geoms
+#' (full "eye") versions. See \code{\link{geom_density_ridges}} and \code{\link{stat_summaryh}} for the geoms
 #' this function is based on.
 #' @keywords manip
 #' @examples
@@ -65,7 +65,7 @@
 #'
 #' @importFrom utils modifyList
 #' @importFrom ggstance stat_summaryh geom_pointrangeh
-#' @importFrom ggjoy geom_joy
+#' @importFrom ggridges geom_density_ridges
 #' @import ggplot2
 #' @export
 geom_halfeyeh = function(
@@ -73,7 +73,7 @@ geom_halfeyeh = function(
   mapping = NULL, data = NULL,
 
   #deity properties
-  stat = "joy", position = "identity", scale = 0.9, rel_min_height = 0, fill = "gray65",
+  stat = "density_ridges", position = "identity", scale = 0.9, rel_min_height = 0, fill = "gray65",
 
   ...,
 
@@ -92,7 +92,7 @@ geom_halfeyeh = function(
     ) %>%
     {if (!is.null(fill)) modifyList(., list(fill = fill)) else .}
 
-  joy = do.call(geom_joy, density.args)
+  dens = do.call(geom_density_ridges, density.args)
 
   #build interval annotations
   interval.args =
@@ -108,5 +108,5 @@ geom_halfeyeh = function(
   # > ggplot(...) + list(geom_a(), geom_b())
   # which is equivalent to
   # > ggplot(...) + geom_a() + geom_b()
-  list(joy, interval)
+  list(dens, interval)
 }
