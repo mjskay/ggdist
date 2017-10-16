55f3768388cc9bb0f42e0e05f6d77fd98b386662
Matthew Kay <matthew.kay@gmail.com>
2017-10-15 23:12:12 -0400

    doc update

--- R/geom_eye.R
+++ R/geom_eye.R
@@ -67,7 +67,19 @@
 #' @keywords manip
 #' @examples
 #'
-#' #TODO
+#' library(magrittr)
+#' library(ggplot2)
+#' data(RankCorr)
+#'
+#' RankCorr %>%
+#'   spread_samples(u_tau[i]) %>%
+#'   ggplot(aes(y = i, x = u_tau)) +
+#'   geom_eyeh()
+#'
+#' RankCorr %>%
+#'   spread_samples(u_tau[i]) %>%
+#'   ggplot(aes(x = i, y = u_tau)) +
+#'   geom_eye()
 #'
 #' @importFrom utils modifyList
 #' @importFrom purrr discard
