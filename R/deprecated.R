c81ccf6d54be094fa4c141f005694b84a91423e7
Matthew Kay <matthew.kay@gmail.com>
2017-11-02 23:44:38 -0400

    minor doc fixes

--- R/deprecated.R
+++ R/deprecated.R
@@ -11,14 +11,17 @@
 #'
 #' \code{ggeye} is deprecated: I no longer think it is good practice to design
 #' monolithic functions that output ggplot objects; instead, it is more flexible
-#' to design geoms and stats that can be more easily used within a complete ggplot
+#' to design geoms and stats that can used within a complete ggplot
 #' workflow. \code{\link{geom_eyeh}} offers a horizontal eye plot geom that can
 #' be used instad of \code{ggeye}.
+#'
+#' \code{gather_lsmeans_samples} is a deprecated alias for \code{\link{gather_emmeans_samples}}.
+#' The new name (estimated marginal means) is more appropriate for Bayesian models than
+#' the old name (least-squares means).
 #'
 #' @format NULL
 #' @usage NULL
 #' @author Matthew Kay
-#' @seealso \code{\link{geom_eyeh}}
 #' @name tidybayes-deprecated
 #' @import ggplot2
 #' @export
