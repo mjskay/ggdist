cf16cdf4496ef677338b8e948db31c8f745f5fca
Matthew Kay <matthew.kay@gmail.com>
2019-06-02 11:41:40 -0400

    drop tidybayes package load message - it's been long enough

--- R/onAttach.R
+++ R/onAttach.R
@@ -3,10 +3,4 @@
 # Author: mjskay
 ###############################################################################
 
-.onAttach = function(libname, pkgname) {
-  packageStartupMessage (
-    "NOTE: As of tidybayes version 1.0, several functions, arguments, and output column names\n",
-    "      have undergone significant name changes in order to adopt a unified naming scheme.\n",
-    "      See help('tidybayes-deprecated') for more information.\n"
-  )
-}
+# Nothing here at the moment :)
