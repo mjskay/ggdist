6c311c7e214ef7c7a6f055048301b8ad436efe10
Matthew Kay <matthew.kay@gmail.com>
2019-06-01 23:24:47 -0400

    initial test for residual_draws

---
+++ tests/testthat/test.residual_draws.R
@@ -0,0 +1,39 @@
+# Tests for [add_]residual_draws
+#
+# Author: mjskay
+###############################################################################
+
+library(dplyr)
+library(tidyr)
+
+context("residual_draws")
+
+
+# data
+mtcars_tbl = mtcars %>%
+  set_rownames(seq_len(nrow(.))) %>%
+  as_tibble()
+
+
+test_that("[add_]residual_draws works on a simple brms model", {
+  skip_if_not_installed("brms")
+  m_hp = readRDS("../models/models.brms.m_hp.rds")
+
+  resids = residuals(m_hp, mtcars_tbl, summary = FALSE) %>%
+    as.data.frame() %>%
+    set_names(seq_len(ncol(.))) %>%
+    mutate(
+      .chain = NA_integer_,
+      .iteration = NA_integer_,
+      .draw = seq_len(n())
+    ) %>%
+    gather(.row, .residual, -.chain, -.iteration, -.draw) %>%
+    as_tibble()
+
+  ref = mtcars_tbl %>%
+    mutate(.row = rownames(.)) %>%
+    inner_join(resids, by = ".row") %>%
+    mutate(.row = as.integer(.row))
+
+  expect_equal(residual_draws(m_hp, mtcars_tbl), ref)
+})
