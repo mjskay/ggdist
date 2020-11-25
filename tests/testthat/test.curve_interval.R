5db408205c1e5263e84eaf057199de37a6743ddf
Matthew Kay <matthew.kay@gmail.com>
2020-11-25 00:51:32 -0600

    tests for curve_interval

---
+++ tests/testthat/test.curve_interval.R
@@ -0,0 +1,46 @@
+# Tests for curve_interval
+#
+# Author: mjskay
+###############################################################################
+
+library(dplyr)
+library(tidyr)
+
+context("curve_interval")
+
+
+test_that("curve_interval works with lineribbon", {
+  skip_if_not_installed("vdiffr")
+  skip_if_not_installed("svglite")
+
+  k = 11 # number of curves
+  n = 101
+  curve_df = tibble(
+    .draw = 1:k,
+    mean = seq(-5,5, length.out = k),
+    x = list(seq(-15,15,length.out = n)),
+  ) %>%
+    unnest(x) %>%
+    mutate(y = dnorm(x, mean, 3)/max(dnorm(x, mean, 3))) %>%
+    group_by(x)
+
+
+  vdiffr::expect_doppelganger("curve_interval with mhd",
+    curve_df %>%
+      curve_interval(y, .width = c(.5, .8), .interval = "bd-mbd") %>%
+      ggplot(aes(x = x, y = y)) +
+      geom_lineribbon(aes(ymin = .lower, ymax = .upper)) +
+      geom_line(aes(group = .draw), alpha = 0.15, data = df) +
+      scale_fill_brewer()
+  )
+
+  vdiffr::expect_doppelganger("curve_interval with bd-mbd",
+    curve_df %>%
+      curve_interval(y, .width = c(.5, .8), .interval = "bd-mbd") %>%
+      ggplot(aes(x = x, y = y)) +
+      geom_lineribbon(aes(ymin = .lower, ymax = .upper)) +
+      geom_line(aes(group = .draw), alpha = 0.15, data = df) +
+      scale_fill_brewer()
+  )
+
+})
