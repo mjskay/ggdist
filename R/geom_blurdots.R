85ca9efff850af3926f6542d960955959845912d
Matthew Kay <matthew.kay@gmail.com>
2022-07-21 01:38:19 -0500

    blurry dotplot using radial gradient

--- R/geom_blurdots.R
+++ R/geom_blurdots.R
@@ -7,11 +7,17 @@
 
 # blur functions ---------------------------------------------------------------
 
-blur_alpha_1 = function(n, r, sd) {
+blur_alpha_0 = function(...) {
+  blur_alpha_1(..., rescale = TRUE)
+}
+
+blur_alpha_1 = function(n, r, sd, min_alpha = 0.1, rescale = FALSE) {
   x = seq(0, max(2*sd, r), length.out = n)
   dens = dnorm(x, 0, sd) / dnorm(0, 0, sd)
   weight = 1 - pnorm(x, r, max(sd - abs(r - sd), 0) / 2)
-  weight + (1 - weight) * dens
+  f = weight + (1 - weight) * dens
+  if (rescale) f = pmax(f * dnorm(0, 0, sd) / dnorm(0, 0, min(sd, r/2)), min_alpha)
+  f
 }
 
 blur_alpha_2 = function(n, r, sd) {
@@ -32,22 +38,29 @@ blur_alpha_3 = function(n, r, sd) {
   }
 }
 
-blur_dot = function(x = 0.5, y = 0.5, r = unit(0.5 ,"npc"), sd = unit(0.25, "npc"), n = 20, fill = "black", vp = NULL, blur_alpha = blur_alpha_2) {
+blur_alpha_4 = function(n, r, sd, min_alpha = 0.1) {
+  x = seq(0, max(2*sd, r), length.out = n)
+  dens = dnorm(x, 0, sd) / dnorm(0, 0, min(sd, r/2))
+  weight = 1 - pnorm(x, r, max(sd - abs(r - sd), 0) / 2)
+  pmax(weight + (1 - weight) * dens, min_alpha)
+}
+
+blur_dot = function(x = 0.5, y = 0.5, r = unit(0.5 ,"npc"), sd = unit(0.25, "npc"), n = 20, fill = "black", col = NA, lwd = 1, lty = "solid", vp = NULL, blur_alpha = blur_alpha_1) {
   # r = convertWidth(r, unitTo = "npc")
   # sd = convertWidth(sd, unitTo = "npc")
 
-  do.call(grobTree, .mapply(list(x, y, fill), NULL, FUN = function(x, y, fill) {
-    circ = if (as.numeric(r) >= 2 * as.numeric(sd)) {
-      circleGrob(x = x, y = y, gp = gpar(fill = fill), r = r)
-    } else {
+  groupGrob(do.call(grobTree, .mapply(list(x, y, fill, sd, lwd, lty), NULL, FUN = function(x, y, fill, sd, lwd, lty) {
+    sd = unit(sd, "points")
+    # circ = if (as.numeric(r) >= 2 * as.numeric(sd)) {
+      # circleGrob(x = x, y = y, gp = gpar(fill = fill, col = NA), r = r)
+    # } else {
       grad_colors = alpha(fill, c(blur_alpha(n, as.numeric(r), as.numeric(sd)), 0))
-      grad = radialGradient(grad_colors, r2 = sd*2)
-      rectGrob(x = x, y = y, gp = gpar(fill = grad, col = NA), height = 2*r, width = max(4*sd, 2*r))
-    }
-    # grobTree(
-      # circ,
-      # circleGrob(x = x, y = y, gp = gpar(col = "red", fill = NA), r = r)
-    # )
-    circ
-  }))
+      grad = radialGradient(grad_colors, r2 = max(2*sd, r))
+      circ = rectGrob(x = x, y = y, gp = gpar(fill = grad, col = NA), height = 2*r, width = max(4*sd, 2*r))
+    # }
+    grobTree(
+      circ,
+      circleGrob(x = x, y = y, gp = gpar(col = col, fill = NA, lwd = lwd, lty = lty), r = r)
+    )
+  })), op = "over")
 }
