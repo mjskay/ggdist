e901576a821ec5ff520896eabbf99ad85301ae60
Matthew Kay <matthew.kay@gmail.com>
2023-03-08 17:48:32 -0600

    simplify stat_cdf test

--- tests/testthat/test.stat_cdfinterval.R
+++ tests/testthat/test.stat_cdfinterval.R
@@ -11,14 +11,13 @@ test_that("dodged ccdf barplots work", {
   skip_if_no_vdiffr()
 
 
-  set.seed(123)
-  df = data.frame(y = rnorm(500, 1), x = "a", g = c("g1")) %>%
-    rbind(data.frame(y = rnorm(900), x = "b", g = c("g1", "g2", "g3")))
+  df = data.frame(y = 1:5, x = "a", g = c("g1")) %>%
+    rbind(data.frame(y = rep(1:5, each = 3) + 1:3, x = "b", g = c("g1", "g2", "g3")))
 
   p = ggplot(df, aes(x = x, y = y))
 
   vdiffr::expect_doppelganger("vertical CCDF bar plot with dodging (3 groups)",
-    p + stat_ccdfinterval(aes(fill = g), position = "dodge", n = 20)
+    p + stat_ccdfinterval(aes(fill = g), position = "dodge", n = 10)
   )
 
 })
