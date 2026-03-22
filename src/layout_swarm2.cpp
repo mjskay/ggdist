f4a9f71ab9bc6e504d01db905fbc49146f0eda9b
Matthew Kay <matthew.kay@gmail.com>
2026-03-22 16:11:31 -0500

    Factor out reversible sequence helpers

--- src/layout_swarm2.cpp
+++ src/layout_swarm2.cpp
@@ -10,102 +10,9 @@
 #include <set>
 #include <vector>
 
+// stratified swarm helpers ------------------------------------------------------
+
 namespace {
-
-// reversible sequence helpers ------------------------------------------------------
-
-// These helpers allow us to write the core grid swarm placement methods in a way that
-// is agnostic to whether we are iterating forward or backward through the candidate dots.
-
-/// const begin iterator for forward or reverse iteration
-/// @tparam reverse iterate in reverse?
-/// @tparam C container type
-/// @param container object to iterate over
-template<bool reverse, typename C>
-inline auto cbegin_(const C& container) {
-  if constexpr (reverse) {
-    return container.crbegin();
-  } else {
-    return container.cbegin();
-  }
-}
-
-/// const end iterator for forward or reverse iteration
-/// @tparam reverse iterate in reverse?
-/// @tparam C container type
-/// @param container object to iterate over
-template<bool reverse, typename C>
-inline auto cend_(const C& container) {
-  if constexpr (reverse) {
-    return container.crend();
-  } else {
-    return container.cend();
-  }
-}
-
-/// Erase an element from a container via a (possibly reversed) iterator
-/// @tparam reverse is the iterator reversed?
-/// @tparam C container type
-/// @tparam It iterator type
-/// @param container object to erase from
-/// @param it iterator pointing at element to erase
-template<bool reverse, typename C, typename It>
-inline auto erase_(C& container, const It& it) -> It {
-  if constexpr (reverse) {
-    return std::reverse_iterator(container.erase(std::next(it).base()));
-  } else {
-    return container.erase(it);
-  }
-}
-
-/// Add distance to a value, possibly in reverse direction
-/// @tparam reverse add in reverse direction?
-/// @param value base value
-/// @param distance distance to add
-/// @returns value plus (or minus) distance
-template<bool reverse>
-inline auto add_distance(const double value, const double distance) -> double {
-  if constexpr (reverse) {
-    return value - distance;
-  } else {
-    return value + distance;
-  }
-}
-
-/// Directional minimum of candidate positions
-/// @tparam reverse take maximum instead of minimum?
-/// @param a first candidate
-/// @param b second candidate
-/// @returns minimum (or maximum) of `a` and `b`
-template<bool reverse>
-inline auto min_candidate(const double a, const double b) -> double {
-  if constexpr (reverse) {
-    return std::max(a, b);
-  } else {
-    return std::min(a, b);
-  }
-}
-
-/// Advance an iterator on a set of candidates to at least `min_next_candidate`
-/// @tparam reverse is the iterator reversed?
-/// @tparam Candidates container type
-/// @param candidates set of candidates
-/// @param it current iterator position
-/// @param min_next_candidate minimum candidate value to advance to
-template<bool reverse, typename Iterator, typename Candidates>
-inline auto advance_to_at_least(
-  const Candidates& candidates, const Iterator it, const double min_next_candidate
-) {
-  if constexpr (reverse) {
-    auto next_it = std::reverse_iterator(upper_bound_(candidates, min_next_candidate));
-    return std::max(it, next_it);
-  } else {
-    auto next_it = lower_bound_(candidates, min_next_candidate);
-    return std::max(it, next_it);
-  }
-}
-
-// core grid swarm placement methods ------------------------------------------------------
 
 /// Attempt to place a candidate dot in a target row
 /// @tparam Row container type for rows of already-placed dots
@@ -145,12 +52,12 @@ inline auto place_candidate(
     const auto y_offset = rows_from_target / static_cast<double>(ygrid);
     const auto min_x_dist = std::sqrt(1 - y_offset * y_offset) * (xsize - eps);
 
-    auto loc = upper_bound_(row, candidate);
+    auto loc = row.upper_bound(candidate);
     if (loc != row.end()) {
       const auto min_val_gt_candidate = *loc;
       if (candidate > min_val_gt_candidate - min_x_dist) {
         // overlap => can't place candidate here
-        min_next_candidate = add_distance<reverse>(min_val_gt_candidate, min_x_dist);
+        min_next_candidate = min_val_gt_candidate + negate_if<reverse>(min_x_dist);
         return false;
       }
     }
@@ -158,7 +65,7 @@ inline auto place_candidate(
       const auto max_val_lte_candidate = *std::prev(loc);
       if (candidate < max_val_lte_candidate + min_x_dist) {
         // overlap => can't place candidate here
-        min_next_candidate = add_distance<reverse>(max_val_lte_candidate, min_x_dist);
+        min_next_candidate = max_val_lte_candidate + negate_if<reverse>(min_x_dist);
         return false;
       }
     }
@@ -169,7 +76,7 @@ inline auto place_candidate(
   }
 
   target_row.insert(insert_loc, candidate);
-  min_next_candidate = add_distance<reverse>(candidate, xsize - eps);
+  min_next_candidate = candidate + negate_if<reverse>(xsize - eps);
   return true;
 }
 
@@ -235,11 +142,11 @@ inline auto place_row(
 
     // skip candidates that are definitely not placeable
     if (place_both) {
-      min_next_candidate = min_candidate<reverse>(min_next_candidate_top, min_next_candidate_bottom);
+      min_next_candidate = min_<reverse>(min_next_candidate_top, min_next_candidate_bottom);
     } else {
       min_next_candidate = min_next_candidate_top;
     }
-    it = advance_to_at_least<reverse>(candidates, it, min_next_candidate);
+    it = advance_to_at_least(candidates, it, min_next_candidate);
   }
 
   ++row_num;
--- src/util.hpp
+++ src/util.hpp
@@ -5,7 +5,7 @@
 
 #include <algorithm>
 #include <limits>
-#include <set>
+#include <type_traits>
 
 // constants --------------------------------------------------------------------------
 
@@ -42,22 +42,98 @@ constexpr auto ssize_(const C& c) -> std::common_type_t<std::ptrdiff_t, std::mak
     return static_cast<signed_c_size_t>(c.size());
 }
 
-/// Upper bound for a value in a container
-template<class C>
-inline auto upper_bound_(C& container, const double value) {
-  return std::upper_bound(container.begin(), container.end(), value);
-}
-template<>
-inline auto upper_bound_<std::multiset<double>>(std::multiset<double>& container, const double value) {
-  return container.upper_bound(value);
+// reversible sequence helpers ------------------------------------------------------
+
+// These helpers allow us to write the core grid swarm placement methods in a way that
+// is agnostic to whether we are iterating forward or backward through the candidate dots.
+
+/// const begin iterator for forward or reverse iteration
+/// @tparam reverse iterate in reverse?
+/// @tparam C container type
+/// @param container object to iterate over
+template<bool reverse, typename C>
+inline auto cbegin_(const C& container) {
+  if constexpr (reverse) {
+    return container.crbegin();
+  } else {
+    return container.cbegin();
+  }
 }
 
-/// Lower bound for a value in a container
-template<class C>
-inline auto lower_bound_(C& container, const double value) {
-  return std::lower_bound(container.begin(), container.end(), value);
+/// const end iterator for forward or reverse iteration
+/// @tparam reverse iterate in reverse?
+/// @tparam C container type
+/// @param container object to iterate over
+template<bool reverse, typename C>
+inline auto cend_(const C& container) {
+  if constexpr (reverse) {
+    return container.crend();
+  } else {
+    return container.cend();
+  }
 }
-template<>
-inline auto lower_bound_<std::multiset<double>>(std::multiset<double>& container, const double value) {
-  return container.lower_bound(value);
+
+/// Erase an element from a container via a (possibly reversed) iterator
+/// @tparam reverse is the iterator reversed?
+/// @tparam C container type
+/// @tparam It iterator type
+/// @param container object to erase from
+/// @param it iterator pointing at element to erase
+template<bool reverse, typename C, typename It>
+inline auto erase_(C& container, const It& it) -> It {
+  if constexpr (reverse) {
+    return std::reverse_iterator(container.erase(std::next(it).base()));
+  } else {
+    return container.erase(it);
+  }
+}
+
+/// Optionally negate a value
+/// @tparam negate negate the value?
+/// @param value value to negate (if `reverse` is `true`)
+/// @returns `value` or `-value`
+template<bool negate>
+inline auto negate_if(const double value) -> double {
+  if constexpr (negate) {
+    return -value;
+  } else {
+    return value;
+  }
+}
+
+/// Reversible minimum
+/// @tparam reverse take maximum instead of minimum?
+/// @param a first value
+/// @param b second value
+/// @returns minimum (or maximum) of `a` and `b`
+template<bool reverse>
+inline auto min_(const double a, const double b) -> double {
+  if constexpr (reverse) {
+    return std::max(a, b);
+  } else {
+    return std::min(a, b);
+  }
+}
+
+/// Advance an iterator on a container to at least `min_value`
+/// @tparam C container type
+/// @tparam It iterator type (may be const and/or reverse)
+/// @param container sorted container
+/// @param it current iterator position
+/// @param min_value minimum value to advance to
+template<typename C, typename It>
+inline auto advance_to_at_least(
+  C& container, It it, const typename C::value_type min_value
+) -> It {
+  if constexpr (std::is_base_of_v<typename C::reverse_iterator, It>) {
+    return std::reverse_iterator(std::upper_bound(container.begin(), it.base(), min_value));
+  } else if constexpr (std::is_base_of_v<typename C::const_reverse_iterator, It>) {
+    return std::reverse_iterator(std::upper_bound(container.cbegin(), it.base(), min_value));
+  } else if constexpr (std::is_base_of_v<typename C::iterator, It>) {
+    return std::lower_bound(it, container.end(), min_value);
+  } else if constexpr (std::is_base_of_v<typename C::const_iterator, It>) {
+    return std::lower_bound(it, container.cend(), min_value);
+  } else {
+    static_assert(false, "`it` must be an iterator for `container`");
+  }
 }
